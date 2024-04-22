224087d5e565209a4f63efe0af9944331a74d666
gbasaldua <guillermobasalt@gmail.com>
2024-04-22 02:31:09 -0300

    upload file option

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,5 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast",
     "sap/ui/core/UIComponent",
     "gbas/developer/uploadfiles/model/models",
     "gbas/developer/uploadfiles/model/formatter"
@@ -7,7 +9,7 @@ sap.ui.define([
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, UIComponent, Model, formatter) {
+    function (Controller, MessageBox,  MessageToast, UIComponent, Model, formatter) {
         "use strict";
 
         return Controller.extend("gbas.developer.uploadfiles.controller.Home", {
@@ -46,7 +48,58 @@ sap.ui.define([
                 this.getViewData();
             },
 
-            onDownload: function () {
+            onUploadFile: function () {
+                const uriFileSet = this.getOwnerComponent().getModel().sServiceUrl + '/FileSet';
+                const csrfToken = this.getView().getModel().getSecurityToken();
+                var oFileUploader = this.getView().byId("idFileUploader");
+                oFileUploader.setSendXHR(true);
+                //   Se establece la Url del servicio de Upload del Archivo
+                oFileUploader.setUploadUrl(uriFileSet);
+                //   Token para el método POST del archivo del proyecto
+                var oCustomerHeaderToken = new sap.ui.unified.FileUploaderParameter({
+                    name: "x-csrf-token",
+                    value: csrfToken
+                });
+                //   Nombre del archivo a subir
+                var oSlug = new sap.ui.unified.FileUploaderParameter({
+                    name: "slug",
+                    value: oFileUploader.getValue()
+                });
+                //   Se agregar los parámetros del header del request
+                oFileUploader.addHeaderParameter(oCustomerHeaderToken);
+                oFileUploader.addHeaderParameter(oSlug);
+                //   Se realiza el upload del archivo
+                oFileUploader.checkFileReadable().then(function () {
+                    oFileUploader.upload();
+                    oFileUploader.destroyHeaderParameters();
+                }, function (error) {
+                    MessageToast.show("Error en la lectura del archivo");
+                }).then(function () {
+                    oFileUploader.clear();
+                });
+
+            },
+
+            onUploadComplete: function (oEvent) {
+                const sStatus = oEvent.getParameter("status");
+                var sResponse = oEvent.getParameter("responseRaw");
+
+                if (sStatus === 201) {
+                    MessageToast.show("Carga de archivo completada");
+
+                } else {
+                    const errorDetails = jQuery.parseXML(sResponse).querySelector("errordetails");
+                    
+                    if (errorDetails !== null) {
+
+                        const errorMsg = errorDetails.querySelector("message").textContent;
+                        MessageBox.error(errorMsg);
+                    } else
+                        MessageBox.error("Error en la carga del archivo");
+                }
+            },
+
+            onDownloadFile: function () {
                 const oItem= this.getView().byId("idFilesTable").getSelectedItem();
                 const oFile = oItem.getBindingContext("fileList").getObject();
 
